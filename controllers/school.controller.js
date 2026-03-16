8b50ca3637a7cc28f7bad707eefd144bc9ee072b
Yiranubari <yiranubari4@gmail.com>
2026-03-16 12:33:50 +0100

    Implement SchoolController with methods to get and add schools

---
+++ controllers/school.controller.js
@@ -0,0 +1,65 @@
+import schoolService from "../services/school.service.js";
+import { SchoolValidator } from "../validators/school.validator.js";
+
+export class SchoolController {
+  async getSchools(req, res) {
+    try {
+      const { latitude, longitude } = req.query;
+
+      const { error } = SchoolValidator.validateListSchools({
+        latitude: Number(latitude),
+        longitude: Number(longitude),
+      });
+
+      if (error) {
+        return res.status(400).json({
+          success: false,
+          message: error.details[0].message,
+        });
+      }
+
+      const schools = await schoolService.listSchools(
+        Number(latitude),
+        Number(longitude),
+      );
+
+      return res.status(200).json({
+        success: true,
+        data: schools,
+      });
+    } catch (err) {
+      return res.status(500).json({
+        success: false,
+        message: "Internal server error",
+      });
+    }
+  }
+
+  async addSchool(req, res) {
+    try {
+      const { error, value } = SchoolValidator.validateAddSchool(req.body);
+
+      if (error) {
+        return res.status(400).json({
+          success: false,
+          message: error.details[0].message,
+        });
+      }
+
+      const schoolId = await schoolService.addSchool(value);
+
+      return res.status(201).json({
+        success: true,
+        data: { id: schoolId },
+      });
+    } catch (error) {
+      return res.status(500).json({
+        success: false,
+        message: "Internal server error",
+      });
+    }
+  }
+}
+
+const schoolController = new SchoolController();
+export default schoolController;
